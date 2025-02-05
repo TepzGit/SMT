b2b3cf5eac85dcf53cc56a1d2e74884387c3ac5d
TepzGit <90647360+TepzGit@users.noreply.github.com>
2025-02-05 19:59:01 +0100

    Add files via upload

---
+++ ProccessChecker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ProccessorsList="$(dirname $(realpath "$0"))/ProcessList.txt"
+if ! test -f $ProccessorsList;then
+	touch $ProccessorsList
+	echo "Created: $ProccessorsList"
+fi
+
+ProccessorsListContents=$(cat $ProccessorsList)
+LogDirectory="$(dirname $(realpath "$0"))/Logs"
+
+if ! test -d $LogDirectory;then
+	mkdir $LogDirectory
+fi
+
+
+declare -A Running
+for process in $ProccessorsListContents;do
+	Running["$process"]=false
+done
+
+Procceses=$(ps -eo cmd)
+while read -r line; do
+	for process in "${!Running[@]}";do
+		if [[ "$line" == *"$process"* ]] && [[ ${Running["$process"]} != true ]]; then
+			Running["$process"]=true
+			break
+		fi
+	done
+done <<< "$Procceses"
+
+for key in ${!Running[@]};do
+	value=${Running["$key"]}
+	basename=$(basename $key)
+
+	if [[ $value == false ]];then
+		if [[ "$(command -v $key)" != "" ]];then
+			$key > "$LogDirectory/${basename%.*}.log" &
+			echo "Started $key up again"
+		else
+			echo "ERROR: Couldnt find executable: $key"
+		fi
+	fi
+done
